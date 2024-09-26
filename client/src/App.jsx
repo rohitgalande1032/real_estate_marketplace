9c2320d13b5c7e7eaf1e05b42149ecbf2d549b3f
rohitgalande1032 <rohitgalande45@gmail.com>
2024-09-26 17:49:13 +0530

    header created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-6xl'>Hello</h1>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
