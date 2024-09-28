3815f8dfd5c9dd47e9977b0e80c0614dd2a4cb47
rohitgalande1032 <rohitg3234@gmail.com>
2024-09-28 09:08:13 +0530

    profile image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from "../firebase";
+
 function Profile() {
     const {user} = useSelector((state) => state.user)
+    const [file, setFile] = useState(undefined)
+    const [filePerc, setFilePerc] = useState(0)
+    const [fileUploadError, setFileUploadError] = useState(false)
+    const [formData, setFormData] = useState({})
+
+    console.log(formData);
+    
+
+    // console.log(file);
+    // console.log(filePerc)
+    
+    const fileRef = useRef(null)
+    useEffect(() => {
+        if (file) {
+          handleFileUpload(file);
+        }
+    }, [file]);
+
+    const handleFileUpload = (file) => {
+        if (!file) {
+            console.log("No file selected for upload");
+            return;
+        }
+
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + "_" + file.name;
+        const storageRef = ref(storage, fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        // Track progress
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log(`Upload is ${progress}% done`);
+            setFilePerc(Math.round(progress))
+            },
+            (error) => {
+            // Handle unsuccessful uploads
+            setFileUploadError(true)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then
+                ((downLoadUrl) => 
+                    setFormData({...formData, avatar: downLoadUrl}))
+            }
+        )
+    }
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
             <form className="flex flex-col gap-4">
-                <img src={user.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"/>
+                <input type="file" ref={fileRef} onChange={(e)=> setFile(e.target.files[0])} className="hidden" accept="images/*"/>
+                <img onClick={()=>fileRef.current.click()} src={formData.avatar || user.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"/>
+                <p className="text-sm self-center">
+                    {fileUploadError ? (
+                        <span className="text-red-700">Image upload error (image should be less than 2 MB)</span>
+                    ) : filePerc > 0 && filePerc < 100 ? (
+                        <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+                    ) : filePerc == 100 ? (
+                        <span className="text-green-700">Image successfully uploaded</span>
+                    ) : ('')
+                    }
+                </p>
                 <input type="text" placeholder="username" id="username" className="border p-3 rounded-lg"/>
                 <input type="email" placeholder="email" id="email" className="border p-3 rounded-lg"/>
                 <input type="password" placeholder="password" id="password" className="border p-3 rounded-lg"/>
