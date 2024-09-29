54803c11c749335b2500a4034da7d48a597038d2
rohitgalande1032 <rohitg3234@gmail.com>
2024-09-29 09:01:24 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
 
 dotenv.config()
@@ -23,6 +24,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter)
 
 //Middleware for error handling
 app.use((err,req,res,next) => {
