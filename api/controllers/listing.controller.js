576d59df4e12962bc04a2e17a64e70ee63468865
rohitgalande1032 <rohitg3234@gmail.com>
2024-09-30 08:30:28 +0530

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,28 @@
 import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js"
 
 export const CreateListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body)
         return res.status(201).json(listing)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id)
+    if(!listing) {
+        return next(404, errorHandler('Listing not found'))
+    }
+
+    if(req.user._id === listing.userRef) {
+        return next(401, errorHandler('You can only deelete your own listings!'))
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id)
+        res.status(200).json('Listing has been deleted')
     } catch (error) {
         next(error)
     }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { CreateListing } from '../controllers/listing.controller.js'
+import { CreateListing, deleteListing} from '../controllers/listing.controller.js'
 
 const router = express.Router()
 
 router.post('/create', verifyToken, CreateListing)
+router.delete('/delete/:id', verifyToken, deleteListing)
 
 export default router
