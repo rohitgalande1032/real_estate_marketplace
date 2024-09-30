eace5e887174bbd4ee1b31835ce840e7ce235f13
rohitgalande1032 <rohitg3234@gmail.com>
2024-09-30 11:00:45 +0530

    complete update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { CreateListing, deleteListing, updateListing} from '../controllers/listing.controller.js'
+import { CreateListing, deleteListing, updateListing, getListing} from '../controllers/listing.controller.js'
 
 const router = express.Router()
 
 router.post('/create', verifyToken, CreateListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
+router.get('/get/:id', getListing)
 
 export default router
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from './pages/Profile'
 import Header from './components/Header'
 import PrivateRoute from './components/privateRoute'
 import CreateListing  from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/update-listing/:listingId' element={<UpdateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
