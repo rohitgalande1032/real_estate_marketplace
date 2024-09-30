cd6fbf54c1cf82b21a95456e8adbdc50ccd91363
rohitgalande1032 <rohitg3234@gmail.com>
2024-09-30 10:05:36 +0530

    create update listing route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { CreateListing, deleteListing} from '../controllers/listing.controller.js'
+import { CreateListing, deleteListing, updateListing} from '../controllers/listing.controller.js'
 
 const router = express.Router()
 
 router.post('/create', verifyToken, CreateListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
+router.post('/update/:id', verifyToken, updateListing)
 
 export default router
