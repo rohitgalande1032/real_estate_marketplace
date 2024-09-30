b4a6df4c0915f19bd0f260c28bc560c352e87fb2
rohitgalande1032 <rohitg3234@gmail.com>
2024-09-30 14:54:29 +0530

    Create contact landord and contact page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
+import Contact from '../components/Contact';
 import 'swiper/css/bundle';
 import {
   FaBath,
@@ -14,7 +15,6 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
-import Contact from '../components/Contact';
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
@@ -136,7 +136,15 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            
+            {user && listing.userRef !== user._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
