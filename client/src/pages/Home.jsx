fe2c18ad24d60e6e4c7cff8d73ce7a2b506d3aa4
rohitgalande1032 <rohitg3234@gmail.com>
2024-10-01 11:25:56 +0530

    Everything is Done. Ready to deploy

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,15 +50,14 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+          Discover your next <span className='text-slate-500'>perfect</span>
           <br />
-          place with ease
+          place to call Home
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
-          live.
+          RoEstate is the best place to find your next perfect home. Explore a
           <br />
-          We have a wide range of properties for you to choose from.
+          wide range of properties, and discover the ideal space that fits your lifestyle.
         </div>
         <Link
           to={'/search'}
